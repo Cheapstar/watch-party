c2c5a6c21b6af05b9c545f21505418523e4ee970
prasang <prasangblaze@gmail.com>
2025-05-03 13:51:33 +0530

    Stop tracking backend/dist

--- backend/dist/medisoup/eventHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mediasoupEventHandler = mediasoupEventHandler;
-function mediasoupEventHandler({ event, payload, ws, }) {
-    const type = event.split("-")[1];
-    switch (type) {
-        case "sendRtpCapabilities":
-    }
-}
--- backend/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createRoomRouter } from "./room.route.js";
-export function createRootRouter(redisService, roomManager, mediasoupService, wsClient) {
-    const router = Router();
-    router.use("/room", createRoomRouter(redisService, roomManager, mediasoupService, wsClient));
-    return router;
-}
